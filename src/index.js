465e83eb0c7408df3396271a9792fffe29923c7e
Raymond Gregory <vinedsel@gmail.com>
2018-12-04 20:06:29 -0600

    fixed a weird folder problem that was confusing the renderer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const apples = React.createElement(
+  'ul',
+  {},
+  React.createElement('li', {}, '$1.00 per lb')
+);
+const oranges = React.createElement(
+  'ul',
+  {},
+  React.createElement('li', {}, '$.70 per lb')
+);
+const mangos = React.createElement(
+  'ul',
+  {},
+  React.createElement('li', {}, '$2.00 per lb')
+);
+
+ReactDOM.render(
+  React.createElement(
+    'ul',
+    {},
+    React.createElement('li', {}, 'Apples', apples),
+    React.createElement('li', {}, 'Oranges', oranges),
+    React.createElement('li', {}, 'Mangos', mangos)
+  ),
+  document.getElementById('root')
+);
